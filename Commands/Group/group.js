b750c252ce5a89d7ba6e704ab638d548b09f8cd0
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-17 17:35:28 +0100

    Update group.js

--- Commands/Group/group.js
+++ Commands/Group/group.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "group",
     alias: ["gc", "group open"],
-    desc: "Set a group profile picture.",
+    desc: "Legen Sie ein Gruppenprofilbild fest.",
     category: "Group",
-    usage: `Tag an Image and type -setppgc}`,
+    usage: `Markieren Sie ein Bild und geben Sie es ein -setppgc}`,
     react: "🍁",
     start: async (
       Miku,
@@ -14,21 +14,21 @@ module.exports = {
         return Miku.sendMessage(
           m.from,
           {
-            text: `Bot and *${pushName}* both must be admin in order to use this command !`,
+            text: `Bot und *${pushName}* bot muss Administrator sein, um diesen Befehl verwenden zu können !`,
           },
           { quoted: m }
         );
         if (args[0] === 'close'){
-            await Miku.groupSettingUpdate(m.from, 'announcement').then((res) => m.reply(`Group has been closed!`))
+            await Miku.groupSettingUpdate(m.from, 'announcement').then((res) => m.reply(`Gruppe wurde geschlossen!`))
             } else if (args[0] === 'open'){
-            await Miku.groupSettingUpdate(m.from, 'not_announcement').then((res) => m.reply(`Group has been opened!`))
+            await Miku.groupSettingUpdate(m.from, 'not_announcement').then((res) => m.reply(`Gruppe wurde geöffnet!`))
             } else {
             let buttons = [
             { buttonId: '${prefix}group open', buttonText: { displayText: 'Open' }, type: 1 },
             { buttonId: '${prefix}group close', buttonText: { displayText: 'Close' }, type: 1 }
             ]
             let buttonMessage = {
-            text: `*「 ${global.botName} 」*\n\n_Group Setting Changer tool_:`,
+            text: `*「 ${global.botName} 」*\n\n_Gruppen Einstellung Wechsler-Tool_:`,
             footer: `${botName}`,
             buttons: buttons,
             headerType: 4
