9fd65d4df4fd3eaa870d081becaa961a34b4344a
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-17 16:48:27 +0100

    Update screenshot.js

--- Commands/Essentials/screenshot.js
+++ Commands/Essentials/screenshot.js
@@ -3,12 +3,12 @@ const { getBuffer } = require("../../lib/myfunc");
 module.exports = {
     name: "screenshot",
     alias: ["ss"],
-    desc: "Take a screenshot of a website without visiting it.",
+    desc: "Machen Sie einen Screenshot einer Website, ohne sie zu besuchen.",
     usage: "ss <link>",
     react: "🍁",
     category: "Essentials",
     start: async(Miku, m,{pushName,prefix,args,text}) => {       
-        if(!args[0]) return m.reply(`Please provide me a link to lookup !`);
+        if(!args[0]) return m.reply(`Bitte geben Sie mir einen Link zur Suche !`);
         if (!args[0].includes("http")){
             lookupURL= `https://${args[0]}`;
         }
@@ -17,11 +17,9 @@ module.exports = {
         }   
         try {
             const resImage = await getBuffer(`https://api.popcat.xyz/screenshot?url=${lookupURL}`);
-            await Miku.sendMessage(m.from, {image: resImage, caption: `_Here's how this url looks like:_\n${args[0]}\n`}, {quoted: m});
+            await Miku.sendMessage(m.from, {image: resImage, caption: `_So sieht diese URL aus:_\n${args[0]}\n`}, {quoted: m});
         } catch (error) {
-            m.reply(`An error occured while processing your request !\n\nPlease recheck your link and try again !`);
+            m.reply(`Bei der Bearbeitung Ihrer Anfrage ist ein Fehler aufgetreten !\n\nBitte überprüfen Sie Ihren Link und versuchen Sie es erneut !`);
         }
     }
 }
-
-        
