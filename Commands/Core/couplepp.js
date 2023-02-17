8f3c67ded5c0638e2b1abc958884f1eda784e414
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-17 16:28:48 +0100

    Update couplepp.js

--- Commands/Core/couplepp.js
+++ Commands/Core/couplepp.js
@@ -3,14 +3,14 @@ const {fetchJson} = require('../../lib/myfunc')
 module.exports = {
     name: "couplepp",
     alias: ["ppcouple"],
-    desc: "Get matching couple profile picture.",
+    desc: "Holen Sie sich ein passendes Paar-Profilbild.",
     react: "💞",
     category: "Core",
     start: async(Miku, m,{pushName,prefix}) => {
         let anu = await fetchJson('https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json');
         let randompplink = anu[Math.floor(Math.random() * anu.length)];
-        Miku.sendMessage(m.from, { image: { url: randompplink.male }, caption: `_For Him..._` }, { quoted: m })
-        Miku.sendMessage(m.from, { image: { url: randompplink.female }, caption: `_For Her..._` }, { quoted: m })
+        Miku.sendMessage(m.from, { image: { url: randompplink.male }, caption: `_Für ihn..._` }, { quoted: m })
+        Miku.sendMessage(m.from, { image: { url: randompplink.female }, caption: `_Für Sie..._` }, { quoted: m })
 
     }
 }
