bd110381cc71a59e2d8435b5a0e73aede69b011e
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-17 17:40:17 +0100

    Update remove.js

--- Commands/Group/remove.js
+++ Commands/Group/remove.js
@@ -4,7 +4,7 @@ require("../../Core.js");
 module.exports = {
   name: "remove",
   alias: ["rem"],
-  desc: "Remove a member from group",
+  desc: "Entfernen Sie ein Mitglied aus der Gruppe",
   category: "Group",
   usage: "remove @user",
   react: "🍁",
@@ -13,13 +13,13 @@ module.exports = {
     m,
     { text, prefix, isBotAdmin, isAdmin, mentionByTag,pushName}
   ) => {
-    if (!text && !m.quoted) return m.reply(`Please tag a user to *Remove* from group!`)
+    if (!text && !m.quoted) return m.reply(`Bitte markieren Sie einen Benutzer zum *Entfernen* aus der Gruppe!`)
     if (!isAdmin) return Miku.sendMessage(m.from, { text: mess.useradmin }, { quoted: m });
 
     if (!text && !m.quoted) {
       return Miku.sendMessage(
         m.from,
-        { text: `Please tag a user to *Remove* !` },
+        { text: `Bitte markieren Sie einen Benutzer zum *Entfernen* !` },
         { quoted: m }
       );
     } else if (m.quoted) {
@@ -35,7 +35,7 @@ module.exports = {
         (res) =>
           Miku.sendMessage(
             m.from,
-            { text: `User has been *Removed* Successfully by *${pushName}*` },
+            { text: `Benutzer wurde erfolgreich *entfernt* von *${pushName}*` },
             { quoted: m }
           )
       );
