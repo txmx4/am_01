0fef0621920c99ffbd48549d3753fe61d2ae11d2
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-17 16:47:04 +0100

    Update element.js

--- Commands/Essentials/element.js
+++ Commands/Essentials/element.js
@@ -5,31 +5,31 @@ const npt = require("node-periodic-table");
 module.exports = {
     name: "element",
     alias: ["elementinfo"],
-    desc: "To get info of an element from priodic table", 
+    desc: "Um Informationen über ein Element aus der Priodic-Tabelle zu erhalten", 
     usage: "element br",
     react: "🍁",
     category: "Essentials",
     start: async(Miku, m,{pushName,prefix,args,text}) => {
-        if(!args[0]) return m.reply(`Please use this command like this: ${prefix}element br`);
+        if(!args[0]) return m.reply(`Bitte verwenden Sie diesen Befehl wie folgt: ${prefix}element br`);
         const query = args.join(" ");
        const search = await pTable(query);
-       if (search === undefined) return m.reply(`Please provide me a valid element by visiting here !\n\nhttps://en.m.wikipedia.org/wiki/Periodic_table`);
+       if (search === undefined) return m.reply(`Bitte geben Sie mir ein gültiges Element, indem Sie hier vorbeischauen !\n\nhttps://en.m.wikipedia.org/wiki/Periodic_table`);
 
        const response = await npt.getByNumber(search.number);
        let caption  = "";
-        caption = "              *『  Element Details  』*\n\n";
+        caption = "              *『  Elementdetails  』*\n\n";
         caption += `🔴 *Elelment:* ${response.name}\n`;
-		caption += `⬜ *Atomic Number:* ${response.number}\n`;
-		caption += `🟡 *Atomic Mass:* ${response.atomic_mass}\n`;
+		caption += `⬜ *Ordnungszahl:* ${response.number}\n`;
+		caption += `🟡 *Atommasse:* ${response.atomic_mass}\n`;
 		caption += `⬛ *Symbol:* ${response.symbol}\n`;
-		caption += `❓ *Appearance:* ${response.apearance}\n`;
+		caption += `❓ *Aussehen:* ${response.apearance}\n`;
 		caption += `🟢 *Phase:* ${response.phase}\n`;
-		caption += `♨️ *Boiling Point:* ${response.boil} K\n️`;
-		caption += `💧 *Melting Point:* ${response.melt} K\n`;
-		caption += `🟣 *Density:* ${response.density} g/mL\n`;
-		caption += `⚫ *Shells:* ${response.shells.join(", ")}\n`;
+		caption += `♨️ *Siedepunkt:* ${response.boil} K\n️`;
+		caption += `💧 *Schmelzpunkt:* ${response.melt} K\n`;
+		caption += `🟣 *Dichte:* ${response.density} g/mL\n`;
+		caption += `⚫ *Muscheln:* ${response.shells.join(", ")}\n`;
 		caption += `🌐 *URL:* ${response.source}\n\n`;
-		caption += `💬 *Summary:* ${response.summary}\n`;
+		caption += `💬 *Zusammenfassung:* ${response.summary}\n`;
         await Miku.sendMessage(m.from,  {image: {url: botImage1},caption: caption}, {quoted: m });
     }
 }
