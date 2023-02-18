eb023a61cb08831c54bf74678efaab5997b6ce5f
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-18 10:08:41 +0100

    Update block.js

--- Commands/Mods/block.js
+++ Commands/Mods/block.js
@@ -3,7 +3,7 @@ const { mku } = require("../../Database/dataschema.js");
 module.exports = {
     name: "block",
     alias: ["blockuser"],
-    desc: "To block an user from bot's account",
+    desc: "Um einen Benutzer vom Konto des Bots zu sperren",
     category: "Mods",
     usage: "block @user",
     react: "🎀",
@@ -13,12 +13,12 @@ module.exports = {
       { args, text,prefix, isCreator, pushName,modStatus }
     ) => {
       
-      if (modStatus=="false"&&!isCreator)  return Miku.sendMessage(m.from, { text: 'Sorry, only my *Owner* and *Mods* can use this command !' }, { quoted: m });
+      if (modStatus=="false"&&!isCreator)  return Miku.sendMessage(m.from, { text: 'Entschuldigung, nur mein *Owner* und *Mods* können diesen Befehl verwenden !' }, { quoted: m });
 
       if (!text && !m.quoted) {
         return Miku.sendMessage( 
           m.from, 
-          { text: `Please tag a user to *Block*!` }, 
+          { text: `Bitte markieren Sie einen Benutzer zum *Blockieren*!` }, 
           { quoted: m } 
         )}
         else if(m.quoted){
@@ -29,12 +29,12 @@ module.exports = {
         }
 
         await Miku.updateBlockStatus(mentionedUser, 'block').then(async (res) => {
-            Miku.sendMessage(m.from, { text: `Successfully *Blocked* @${mentionedUser.split("@")[0]} Senpai !`, mentions: [mentionedUser] }, { quoted: m }).catch((e)=>{
-                Miku.sendMessage(m.from, { text: `Failed to block @${mentionedUser.split("@")[0]} Senpai ! Maybe he is already blocked !` , mentions: [mentionedUser]}, { quoted: m })
+            Miku.sendMessage(m.from, { text: `Erfolgreich *Blockiert* @${mentionedUser.split("@")[0]} Senpai !`, mentions: [mentionedUser] }, { quoted: m }).catch((e)=>{
+                Miku.sendMessage(m.from, { text: `Fehler beim Blockieren @${mentionedUser.split("@")[0]} Senpai ! Vielleicht ist er schon gesperrt !` , mentions: [mentionedUser]}, { quoted: m })
             }
             )
             }).catch((e)=>{
-                Miku.sendMessage(m.from, { text: `Failed to block @${mentionedUser.split("@")[0]} Senpai ! Maybe he is already blocked !` , mentions: [mentionedUser]}, { quoted: m })
+                Miku.sendMessage(m.from, { text: `Failed to block @${mentionedUser.split("@")[0]} Senpai ! Vielleicht ist er schon gesperrt !` , mentions: [mentionedUser]}, { quoted: m })
             }
             )
     },
