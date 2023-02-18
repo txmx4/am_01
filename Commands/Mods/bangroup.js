1a18574613106b62c783af920fed5cf1e0a99903
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-18 09:58:42 +0100

    Update bangroup.js

--- Commands/Mods/bangroup.js
+++ Commands/Mods/bangroup.js
@@ -8,27 +8,27 @@ const { mk, mku } = require("../../Database/dataschema.js");
 module.exports = {
     name: "bangroup",
     alias: ["bangc"],
-    desc: "Ban a group",
+    desc: "Eine Gruppe sperren",
     category: "core",
     usage: "bangroup",
     react: "🎀",
     start: async (Miku, m, { text, prefix, isBotAdmin, isAdmin, mentionByTag, pushName, isCreator , groupName,modStatus}) => {
     
-    if (modStatus == "false" && !isCreator) return Miku.sendMessage(m.from, { text: 'Sorry, only my *Devs* and *Mods* can use this command !' }, { quoted: m });
+    if (modStatus == "false" && !isCreator) return Miku.sendMessage(m.from, { text: 'Tut mir leid, nur meine *Devs* und *Mods* können diesen Befehl verwenden !' }, { quoted: m });
 
     let checkdata = await mk.findOne({ id: m.from })
     try {
         if (!checkdata) {
             await new mk({ id: m.from, bangroup: "true" }).save()
-            return m.reply(`*${groupName}* is now *Banned* from using *${global.botName}*`)
+            return m.reply(`*${groupName}* ist jetzt für die Verwendung *${global.botName}* *verboten*`)
         } else {
-            if (checkdata.bangroup == "true") return m.reply(`*${groupName}* is *Already Banned* from using *${global.botName}*`)
+            if (checkdata.bangroup == "true") return m.reply(`*${groupName}* ist *bereits verboten* *${global.botName}* zu verwenden`)
             await mk.updateOne({ id: m.from }, { bangroup: "true" })
             return m.reply(`*This Group Is Banned From Using Bot.*`)
         }
     } catch (err) {
         console.log(err);
-        return Miku.sendMessage(m.from, { text: `An internal error occurred while banning the user.` }, { quoted: m });
+        return Miku.sendMessage(m.from, { text: `Beim Sperren des Benutzers ist ein interner Fehler aufgetreten.` }, { quoted: m });
     }
 }
 }
