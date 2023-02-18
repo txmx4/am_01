a65109206be353b17b1a78ac4c9e8b10bd1fe671
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-18 11:12:32 +0100

    Update Maid.js

--- Commands/Weeb/Maid.js
+++ Commands/Weeb/Maid.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 module.exports = {
     name: "maid",
     alias: ["safemaid","smd"],
-    desc: "Get anime maid girls picture.",
+    desc: "Holen Sie sich ein Anime-Mädchen-Bild.",
     react: "🥵",
     category: "Weeb",
     start: async(Miku, m,{pushName,prefix}) => {
@@ -12,13 +12,13 @@ module.exports = {
 var Button = [
       {
         buttonId: `${prefix}smd`,
-        buttonText: { displayText: `>>` },
+        buttonText: { displayText: `Weiter...` },
         type: 1,
       },
     ];
     let neko = {
       image: {url:maids.data.url},
-      caption: `Here I am Oujou...Sama...!!`,
+      caption: `Hier Ich bin Oujou...Sama...!!`,
       footer: `*${botName}*`,
       buttons: Button,
       headerType: 4,
