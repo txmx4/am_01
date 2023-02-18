d31bfbd9637f115dbef3f9f85941856374eded6f
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-02-18 10:14:15 +0100

    Update broadcast.js

--- Commands/Mods/broadcast.js
+++ Commands/Mods/broadcast.js
@@ -8,30 +8,30 @@ const { mk, mku } = require("../../Database/dataschema.js");
 module.exports = {
     name: "broadcast",
     alias: ["bc","bcmessage"],
-    desc: "Broadcast a message in all groups where that bot is present",
+    desc: "Senden Sie eine Nachricht an alle Gruppen, in denen dieser Bot vorhanden ist",
     category: "Mods",
     usage: "bc <message>",
     react: "🎀",
     start: async (Miku, m, { text, prefix, isBotAdmin, participants, pushName, isCreator , args,modStatus}) => {
     
-    if (modStatus == "false" && !isCreator) return Miku.sendMessage(m.from, { text: 'Sorry, only my *Devs* and *Mods* can use this command !' }, { quoted: m });
+    if (modStatus == "false" && !isCreator) return Miku.sendMessage(m.from, { text: 'Tut mir leid, nur meine *Devs* und *Mods* können diesen Befehl verwenden !' }, { quoted: m });
 
-    if(!text && !m.quoted) return Miku.sendMessage(m.from, { text: `Please provide a message to broadcast !\n\nExample: ${prefix}bc Hello everyone!` }, { quoted: m });
+    if(!text && !m.quoted) return Miku.sendMessage(m.from, { text: `Bitte geben Sie eine Nachricht zur Übertragung ein !\n\nBeispiel: ${prefix}bc Hallo Leute!` }, { quoted: m });
 
     const broadcastText = m.quoted ? m.quoted.msg : args ? args.join(" ") : "" ;
 
     let FetchGC = await Miku.groupFetchAllParticipating()
     let group = Object.entries(FetchGC).slice(0).map(entry => entry[1])
     let anu = group.map(v => v.id)
-    m.reply(`*Broadcasting message to ${anu.length} groups...*`)
+    m.reply(`*Broadcast-Nachricht an ${anu.length} gruppen...*`)
 
     for (let i of anu) {
-        let txt = `*「  🧣 ${botName} Broadcast 🧣  」*\n\n*🧩 Message:* ${broadcastText}\n\n\n*🔰 Regards ~ ${pushName}*`
+        let txt = `*「  🧣 ${botName} Broadcast 🧣  」*\n\n*🧩 Nachricht:* ${broadcastText}\n\n\n*🔰 Grüße ~ ${pushName}*`
         setTimeout(function(){
             Miku.sendMessage(i, {video:botVideo, caption: txt, mentions:participants.map(a => a.id)});
         }, 1500);
     }
 
-    m.reply(`*Broadcasting message to ${anu.length} groups completed !*`)
+    m.reply(`*Broadcast-Nachricht an ${anu.length} Gruppen Gesendet !*`)
 }
 }
